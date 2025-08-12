dd527a487eaec942f968e4ec10fbb30ffb66cef2
Robin Müller <R.Mueller.inf23@stud.bbbaden.ch>
2025-08-12 17:24:26 +0200

    refactored Item to Todo

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ import helper
 from flask import Flask, request, Response, render_template, redirect, url_for
 app = Flask(__name__)
 
+# Hier wird definiert, auf welche URLs die Applikation reagiert
 @app.route("/")
 def index():
-    items = helper.get_all()
-    return render_template('index.html', items=items)
+    todos = helper.get_all()
+    return render_template('index.html', todos=todos)
 
-
+# Hier findet die Ver-BBB-isierung statt
+# Hier werden die Daten gespeichert
 @app.route('/add', methods=["POST"])
 def add():
-    text = request.form.get("text")
-    helper.add(text)
+    title = request.form.get("title")
+    helper.add(title)
     return redirect(url_for("index"))
 
-
+# Hier werden die Daten an die index.html übergeben
 @app.route('/update/<int:index>')
 def update(index):
     helper.update(index)
