cf2b2b0e37dc1e61eb961216689fc56759a3c18f
Robin Müller <R.Mueller.inf23@stud.bbbaden.ch>
2025-08-26 14:17:19 +0200

    Asks the user for date, closes #3

--- helper.py
+++ helper.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import datetime
 
 todos = []
 
@@ -6,12 +7,17 @@ todos = []
 @dataclass
 class Todo:
     title: str
+    date: datetime
     isCompleted: bool = False
 
 
-def add(title):
+def add(title, date=None):
     title = title.replace('b', 'bbb').replace('B', 'Bbb')
-    todos.append(Todo(title))
+    if date is None:
+        date = datetime.date.today()
+    elif isinstance(date, str):
+        date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+    todos.append(Todo(title, date))
 
 
 def get_all():
---
+++ test_helper.py
@@ -0,0 +1,16 @@
+import pytest
+import helper
+import datetime
+
+
+def test_add():
+    # Given: I want to add a to-do with a date
+    text = "Lorem ipsum"
+    date = "2023-09-02"
+
+    # When: I add the item
+    helper.add(text, date)
+
+    # Then: The most recently added to-do should have a date
+    ToDo = helper.todos[-1]
+    assert isinstance(ToDo.date, datetime.date)
